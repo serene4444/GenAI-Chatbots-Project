42e8f148d8a56b1eba499ff19249e86b1ca2c9aa
Serene Plummer <MercuryRetro84@outlook.com>
2025-07-07 17:57:09 -0500

    need fixing

--- PROJECT/server.py
+++ PROJECT/server.py
@@ -40,4 +40,4 @@ async def query(item: Item):
     return{"response": str(result)}
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host = "127.0.0.1", port = 8000, reload = True)
+    uvicorn.run("main:app", host = "127.0.0.1", port = 8000, reload = True)
